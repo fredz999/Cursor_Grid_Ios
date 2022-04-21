fd7ddd2454b205b52e970546fc61d30cc04d7a58
fredz999 <jjjj58@hotmail.com>
2022-04-21 11:45:16 +0100

    Initial Commit

---
+++ Pattern_Grid_Ios_Proj_2/Pattern_Grid_Ios_Proj_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pattern_Grid_Ios_Proj_2App.swift
+//  Pattern_Grid_Ios_Proj_2
+//
+//  Created by Jon on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pattern_Grid_Ios_Proj_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
